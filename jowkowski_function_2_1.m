dd57434c9e4f82c697095db2e61e040c72ceac34
krassool <krassool@student.unimelb.edu.au>
2017-04-26 18:49:57 +1000

    moving on to 2_1 and 1_2, new scirpts... leave the others in working condition

--- jowkowski_function_2_1.m
+++ jowkowski_function_2_1.m
@@ -1,10 +1,18 @@
-function sorted_panels = jowkowski_function_3_0(alpha_deg)
+function clipped_panels = jowkowski_function_2_0(alpha_deg)
 
 %% set initial conditions
-a   = 1;
-c   = 0.95;
-x_s =-0.04875;
-y_s =0.05*i;
+a=1;
+c=0.95;
+x_s=-0.04875;
+y_s=0.05*i;
+
+%% set temp initial conditions
+a=1;
+c=0.00;%0.95;
+x_s=0.00%-0.04875;
+y_s=0.0;%0.05*i;
+alpha_deg=0; %degrees
+
 
 %% create physical variables
 alpha=alpha_deg/180*pi %angle of attack in radians
@@ -46,6 +54,23 @@ for j=1:n
 end
 
 flip_panels=flip(all_panels);
-sorted_panels=circshift(flip_panels,-2);
+sorted_panels=circshift(flip_panels,-1);
+clipped_panels=sorted_panels(1:end-1,:);
+
+%% Plotting (optional)
+
+
+%% Plotting
+
+% figure ; hold on ;
+% for p=1:n
+%    Xj = [clipped_panels(p,1),clipped_panels(p,3)]
+%    Yj = [clipped_panels(p,2),clipped_panels(p,4)]
+%    plot(Xj,Yj,'-b','LineWidth', 2.5)
+%    pause
+% end
+
 
 end
+
+
