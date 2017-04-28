115315b28720f422366f05312885ca9db4dc2227
krassool <krassool@student.unimelb.edu.au>
2017-04-28 19:07:03 +1000

    yeeeep

---
+++ Source_Panel_Single_Solver.m
@@ -0,0 +1,47 @@
+% this is an example code to produce the right-hand-plot of figure 5
+try close figsou
+end
+figpan=figure;
+
+
+U_inf=1;
+% make a mesh of x and y positions
+[xp yp] = meshgrid([-10.1:.5:10.1], [-10.1:.5:10.1]);
+q = 1; % source panel strength density
+
+Xj = [-3 3]; % start and end points x and y coords
+Yj = [0 0];
+Xmj = 0.5*(Xj(1) + Xj(2)); % midpoints
+Ymj = 0.5*(Yj(1) + Yj(2));
+% equation (13)
+rij = sqrt((Xmj - xp).^2 + (Ymj - yp).^2);
+% equation (14) and (15)
+Phi = atan2((Yj(2) - Yj(1)),(Xj(2) - Xj(1)));
+beta = atan2((yp - Ymj),(xp - Xmj));
+omega = beta - Phi;
+% equations (16) and (17)
+x0p = rij.*cos(omega);
+y0p = rij.*sin(omega);
+% equation (11 & 12)
+S = sqrt((Xj(2) - Xj(1)).^2 + (Yj(2) - Yj(1)).^2);
+a = -S/2;
+b = S/2;
+% equations (18) and (19)
+vprime = (q./(2*pi)).*(atan(((S/2) - x0p)./y0p) - atan((-(S/2) - x0p)./y0p));
+uprime = (q./(2*pi)).*((-log((y0p.^2 + ((S.^2)/4) - (S.*x0p) + x0p.^2))./2) ...
+    - (-log((y0p.^2 + ((S.^2)/4) + (S.*x0p) + x0p.^2))./2));
+% equations (21) and (22)
+v = vprime.*cos(Phi) + uprime.*sin(Phi)             ;
+u = uprime.*cos(Phi) - vprime.*sin(Phi)     +U_inf  ;
+
+% plot the panel
+plot(Xj, Yj, 'b-', 'LineWidth', 2.5)
+hold on
+% plot the vectors
+quiver(xp, yp, u, v, 1, 'k')
+daspect([1 1 1])
+axis([-10 10 -10 10])
+xlabel('x')
+ylabel('y')
+
+
